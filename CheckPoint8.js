264062b1933d356c8f55f6ec8aae8b1c74c01444
Maider88 <maiarrati@gmail.com>
2025-04-10 20:41:42 +0200

    CheckPoint8.js

---
+++ CheckPoint8.js
@@ -0,0 +1,17 @@
+const miLista = ["velma", "exploradora", "jane", "john", "harry"];
+
+for (let i = 0; i < miLista.length; i++) {
+    console.log(miLista[i]);
+}
+
+let contador = 0;
+
+while (contador < miLista.length) {
+    console.log(miLista[contador]);
+    contador++;
+}
+
+
+const holaMundo = () => "Hola mundo";
+
+console.log(holaMundo());
